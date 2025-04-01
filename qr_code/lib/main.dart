526e51fe1a32e784bf142c96edd6baa8483a6974
AhmedAbdelgawadHassan <ahmedabdelgawad.developer@gmail.com>
2025-04-01 17:00:35 +0200

    Create Project

---
+++ qr_code/lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+void main ()
+{
+  runApp(Qr_Code());
+}
+
+
+class Qr_Code extends StatelessWidget {
+  const Qr_Code({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp();
+  }
+}
